c025d0f34c0998d99960d86576282141172dca5c
M.Abdelrady <M.Abdelrady@192.168.1.9>
2020-02-20 17:26:29 +0200

    divide test

--- simple calculator/src/divide.java
+++ simple calculator/src/divide.java
@@ -4,9 +4,14 @@ import org.junit.jupiter.api.Test;
 
 class divide {
 
+	
+
+	private static final Float Error = null;
+
 	@Test
 	void test() {
 		SimpleCalculator input=new SimpleCalculator();
+		
 		try {
 			assertEquals(0.8f, input.divide(4, 5));
 			assertEquals(1.25, input.divide(5, 4));
@@ -14,6 +19,7 @@ class divide {
 			assertEquals(-0.8f, input.divide(-4, 5));
 			assertEquals(-1.25, input.divide(-5, 4));
 			assertEquals(-1, input.divide(-5, 5));
+			assertEquals("ArithmeticException", input.divide(5, 0));
 		} catch (ArithmeticException e) {
 			System.out.println(e.toString());
 		}
